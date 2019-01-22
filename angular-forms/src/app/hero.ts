07e4671361ee3a400311036ed1085125fff0d497
Vijayakumar59 <justvijaya059@gmail.com>
2019-01-22 16:16:54 +0530

    angular-form added

---
+++ angular-forms/src/app/hero.ts
@@ -0,0 +1,12 @@
+export class Hero {
+
+    constructor(public id: number,
+                public name: String,
+                public power: String,
+                public alterEgo?: String)
+                {
+                   
+    }
+
+   
+}
